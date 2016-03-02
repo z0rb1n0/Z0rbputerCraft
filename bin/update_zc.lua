9bb167c2ad3ac26208bdf4b349964be70b677f7d
Fabio Ugo Venchiarutti <fabio@vuole.me>
2016-03-02 22:13:05 +0100

    Added updater

---
+++ bin/update_zc.lua
@@ -0,0 +1,11 @@
+local required_paths = {
+	"/bin", "/lib"
+};
+
+
+local update_me = {
+	["/bin/update_zc.lua"] = "https://raw.githubusercontent.com/z0rb1n0/Z0rbputerCraft/master/lib/navigation.lua",
+	["/lib/navigation.so.lua"] = "https://raw.githubusercontent.com/z0rb1n0/Z0rbputerCraft/master/lib/navigation.lua",
+	["/bin/dig_box.lua"] = "https://raw.githubusercontent.com/z0rb1n0/Z0rbputerCraft/master/bin/dig_box.lua"
+};
+
